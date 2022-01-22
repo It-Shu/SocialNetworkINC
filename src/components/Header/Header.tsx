e8e351ce411587ff65b53f0927be1e7c5561c309
It-Shu <it.shustov@gmail.com>
2022-01-22 22:50:48 +0300

    api log in

--- src/components/Header/Header.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import s from './Header.module.css'
-
-export const Header = () => {
-    return <header className={s.header}>
-        <img src={'https://logocs.ucoz.ru/design/trans_spay_got.jpg'}/>
-    </header>
-}
